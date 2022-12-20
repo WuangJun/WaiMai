eeb94f201098aa31955581e1fcc12bdf07c536e0
wangjun <1581870480@qq.com>
2022-12-20 11:36:12 +0800

    redis优化

--- src/main/java/com/reggie/ReggieApplication.java
+++ src/main/java/com/reggie/ReggieApplication.java
@@ -1,8 +1,11 @@
 package com.reggie;
 
+import com.reggie.entity.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Author:WJ
@@ -11,9 +14,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
+@EnableTransactionManagement
 public class ReggieApplication {
+    /**
+     * 启动函数
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        SpringApplication.run(ReggieApplication.class,args);
+        SpringApplication.run(ReggieApplication.class, args);
         log.info("项目启动成功......");
+
     }
 }
